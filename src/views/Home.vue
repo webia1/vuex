dbb36534a8206c2008adddb61c6f75c2708f3c78
Webia1 <erkan.buelbuel@gmail.com>
2018-04-08 17:02:18 +0200

    watchers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <h1>Home</h1>
     <h2>Counter from state: {{ count }}</h2>
     <h3><router-link to="/about">About</router-link></h3>
+    <h3><router-link to="/watchers">Watchers</router-link></h3>
     <p>
       <button @click="increment">Increment</button>
     </p>
---
+++ src/views/Watchers.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <label for="f1">Field1: </label>
+  <input id="f1" type="text" v-model="f1"/> 
+  <button @click="incrementF1">+</button>
+  <button v-if="f1" @click="decrementF1">-</button>
+  <br/>
+  <label for="f2">Field2: </label>
+  <input id="f2" type="text" v-model="f2" /> 
+  <button @click="incrementF2">+</button>
+  <button v-if="f2" @click="decrementF2">-</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      f1: null,
+      f2: null
+    }
+  },
+  watch: {
+    f1: function(val) {
+      if (!this.f2 || this.f2 < val) this.f2 = val;
+    },
+    f2: function(val) {
+      if (!this.f1 || this.f1 > val) this.f1 = val;
+    },
+  },
+
+  methods: {
+    incrementF1: function () {
+      this.f1 = this.f1 ? ++this.f1 : 1;
+    },
+    incrementF2: function () {
+      this.f2 = this.f2 ? ++this.f2 : 
+        this.f1 ? this.f1 : 1;
+    },
+    decrementF1: function () {
+      this.f1--;
+    },
+    decrementF2: function () {
+      this.f2--;
+    }
+  }
+}
+</script>
