abfbaa86435831a4c98276dc1c777dc5e60baae9
Webia1 <erkan.buelbuel@gmail.com>
2018-04-08 13:57:41 +0200

    lifecycle examples

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>About</h1>
+    <h1>About Now: {{ now }}</h1>
     <h2>Counter from state: {{ count }}</h2>
     <h3><router-link to="/">Home</router-link></h3>
 
@@ -10,13 +10,21 @@
 <script>
 export default {
   name: 'home',
-  mounted: function() {
-    console.log('Mounted!');
-  },
+  beforeCreate:  function () {console.log('beforeCreate!');},
+  created:       function () {console.log('created!');},
+  beforeMount:   function () {console.log('beforeMount!');},
+  mounted:       function () {console.log('mounted!');},
+  beforeUpdate:  function () {console.log('beforeUpdate!');},
+  updated:       function () {console.log('updated!');},
+  beforeDestroy: function () {console.log('beforeDestroy!');},
+  destroyed:     function () {console.log('destroyed!');},
   computed: {
     count() {
       return this.$store.state.vsprobase.count;
     },
+    now () {
+      return Date.now();
+    }
   },
   components: {},
 };
