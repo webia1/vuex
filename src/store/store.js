4d4a9c8e6a402ab9175b45cb38d64a3829dabeaf
Webia1 <erkan.buelbuel@gmail.com>
2018-04-16 22:16:39 +0200

    vuex-persistedstate

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import vsprobase from './base';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     vsprobase,
+    plugins: [createPersistedState({ storage: window.sessionStorage })],
   }
 });
 
