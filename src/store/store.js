a835283653785069367423a9489c7a8e8c2d405a
Webia1 <erkan.buelbuel@gmail.com>
2018-04-29 19:07:31 +0200

    vuex-localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,19 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import vsprobase from './base';
+import createPersist from 'vuex-localstorage';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     vsprobase,
+    plugins: [createPersist({
+      namespace: 'vspro',
+      initialState: {},
+      // ONE DAY 
+      expires: 1 * 24 * 60 * 60 * 1e3
+    })],
   }
 });
 
